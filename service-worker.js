0329e13c40b2bca478a4211497c4d78aa9cb3e8b
Agustín Comolli <agustincomolli@gmail.com>
2026-07-10 22:05:18 -0300

    Actualizar tamaño del ícono a 320x320 y cambiar el nombre de caché a 'reparaciones-cache-v15'

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
  * ==========================================================================
  */
 
-const CACHE_NAME = 'reparaciones-cache-v14';
+const CACHE_NAME = 'reparaciones-cache-v15';
 
 const ASSETS = [
   './',
